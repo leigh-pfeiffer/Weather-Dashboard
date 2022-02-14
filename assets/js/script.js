f4f97abab225595bc701124b651e8dfce6771d8a
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-02-13 23:04:02 -0500

    minor changes to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,18 @@ function initPage() {
     var todayweatherEl = document.getElementById("today-weather");
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-    // Assigning a unique API to a variable
+
     var APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
     function getWeather(cityName) {
-        // Execute a current weather get request from open weather api
+
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
         axios.get(queryURL)
             .then(function (response) {
 
                 todayweatherEl.classList.remove("d-none");
 
-                // Parse response to display current weather
+
                 var currentDate = new Date(response.data.dt * 1000);
                 var day = currentDate.getDate();
                 var month = currentDate.getMonth() + 1;
@@ -36,17 +36,17 @@ function initPage() {
                 currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
                 currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
                 currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
-                
-                // Get UV Index
+
+
                 let lat = response.data.coord.lat;
                 let lon = response.data.coord.lon;
                 let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
                 axios.get(UVQueryURL)
                     .then(function (response) {
                         let UVIndex = document.createElement("span");
-                        
-                        // When UV Index is good, shows green, when ok shows yellow, when bad shows red
-                        if (response.data[0].value < 4 ) {
+
+
+                        if (response.data[0].value < 4) {
                             UVIndex.setAttribute("class", "badge badge-success");
                         }
                         else if (response.data[0].value < 8) {
@@ -60,15 +60,14 @@ function initPage() {
                         currentUVEl.innerHTML = "UV Index: ";
                         currentUVEl.append(UVIndex);
                     });
-                
-                // Get 5 day forecast for this city
+
+
                 let cityID = response.data.id;
                 let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
                 axios.get(forecastQueryURL)
                     .then(function (response) {
                         fivedayEl.classList.remove("d-none");
-                        
-                        //  Parse response to display forecast for next 5 days
+
                         var forecastEls = document.querySelectorAll(".forecast");
                         for (i = 0; i < forecastEls.length; i++) {
                             forecastEls[i].innerHTML = "";
@@ -98,7 +97,7 @@ function initPage() {
             });
     }
 
-    // Get history from local storage if any
+    // Get history from local storage
     searchEl.addEventListener("click", function () {
         var searchTerm = cityEl.value;
         getWeather(searchTerm);
@@ -107,7 +106,7 @@ function initPage() {
         renderSearchHistory();
     })
 
-    // Clear History button
+    // Clear History
     clearEl.addEventListener("click", function () {
         localStorage.clear();
         searchHistory = [];
@@ -137,7 +136,7 @@ function initPage() {
     if (searchHistory.length > 0) {
         getWeather(searchHistory[searchHistory.length - 1]);
     }
-    
+
 }
 
 initPage();
